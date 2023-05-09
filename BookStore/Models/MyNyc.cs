ca5e6b97952e0ebabf9bfba50f465c8998013e7c
nandhini2109 <nandinich2109@gmail.com>
2023-05-09 12:44:38 +0530

    added id into the database Nyc

--- BookStore/Models/MyNyc.cs
+++ BookStore/Models/MyNyc.cs
@@ -5,6 +5,7 @@ namespace BookStore.Models
     public class MyNyc
     {
         [Key]
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
